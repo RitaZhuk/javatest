4e984d42bee98db6d3b384fb74aae71de260b859
RitaZhuk <ritochka199097@gmail.com>
2022-08-06 12:32:51 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -2,6 +2,25 @@
 // Обработка отправки формы form.login-form должна быть по событию submit.
 // При отправке формы страница не должна перезагружаться.
 // Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
-// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект,
+// где имя поля будет именем свойства, а значение поля - значением свойства.
+// Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const loginFormEl = document.querySelector('form.login-form')
+const emailEl = loginFormEl.querySelector('[type = "email"]')
+const passwordEl = loginFormEl.querySelector('[type = "password"]')
+
+loginFormEl.addEventListener('submit', setOutput)
+function setOutput(evt) {
+    evt.preventDefault();
+    const {
+        elements: {email, password},
+    } = evt.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return alert ('Заповніть всі поля');
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    evt.currentTarget.reset();
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,27 @@
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль
+//  при клике на button.change-color и выводит значение цвета в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeColorBtn = document.querySelector('.change-color')
+const colorValue = document.querySelector('.color')
+const bodyEl = document.querySelector('body')
+
+colorValue.textContent = '#ffffff'
+changeColorBtn.addEventListener('click', onChangeColorBtn)
+function onChangeColorBtn(event) {
+  event.preventDefault()
+  bodyEl.style.backgroundColor = getRandomHexColor()
+  colorValue.innerHTML = `${getRandomHexColor}`
+  console.log(getRandomHexColor());
 }
